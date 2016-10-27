69c66be54ee9e353f00e17bfdb2bea156407deb5
Ng Zhi An <ngzhian@gmail.com>
2016-10-27 16:12:10 +0800

    Refactor database helper

--- test/database_helpers.js
+++ test/database_helpers.js
@@ -4,21 +4,40 @@ import { RoomDB } from '../src/database';
 import ROOM_TYPE from '../src/models/room_type';
 
 /**
+ * Creates a room
+ * @param {string} socketId of the creator
+ * @param {object} opts to overwrite defaults
+ * @return {object} promise of creating the room
+ */
+function createRoom(socketId, opts) {
+  const defaults = {
+    roomId: uuid.v4(),
+    roomName: 'Room',
+    roomType: ROOM_TYPE.PUBLIC,
+    userLimit: 100,
+    roomDescription: 'Description',
+    categories: '[]',
+    lastActive: new Date(),
+    numUsers: 1,
+    createdBy: socketId,
+  };
+  const options = {
+    ...defaults,
+    ...opts,
+  };
+
+  return RoomDB.create(options);
+}
+
+/**
  * Creates a HOT room
  * @param {string} socketId of the creator
  * @return {object} promise of creating the room
  */
 function createHotRoom(socketId) {
-  return RoomDB.create({
-    roomId: uuid.v4(),
+  return createRoom(socketId, {
     roomName: 'Hot Room',
     roomType: ROOM_TYPE.HOT,
-    userLimit: 100,
-    roomDescription: 'description',
-    categories: '[]',
-    lastActive: new Date(),
-    numUsers: 0,
-    createdBy: socketId,
   });
 }
 
@@ -28,16 +47,8 @@ function createHotRoom(socketId) {
  * @return {object} promise of creating the room
  */
 function createClosedRoom(socketId) {
-  return RoomDB.create({
-    roomId: uuid.v4(),
-    roomName: 'Room',
-    roomType: ROOM_TYPE.PUBLIC,
-    userLimit: 100,
-    roomDescription: 'description',
-    categories: '[]',
-    lastActive: new Date(),
-    numUsers: 0,
-    createdBy: socketId,
+  return createRoom(socketId, {
+    roomName: 'Closed Room',
     isOpen: false,
   });
 }
@@ -48,17 +59,9 @@ function createClosedRoom(socketId) {
  * @return {object} promise of creating the room
  */
 function createInactiveRoom(socketId) {
-  return RoomDB.create({
-    roomId: uuid.v4(),
-    roomName: 'Room',
-    roomType: ROOM_TYPE.PUBLIC,
-    userLimit: 100,
-    roomDescription: 'description',
-    categories: '[]',
+  return createRoom(socketId, {
+    roomName: 'Inactive Room',
     lastActive: new Date() - 4 * 24 * 60 * 60 * 1000,
-    numUsers: 0,
-    createdBy: socketId,
-    isOpen: true,
   });
 }
 
